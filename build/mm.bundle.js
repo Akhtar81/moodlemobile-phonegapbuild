c4635cdcc53cceb714e2fed102269f74e644222d
Akhtar81 <30456130+Akhtar81@users.noreply.github.com>
2018-03-14 20:20:26 +0600

    Add files via upload

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -11256,7 +11256,10 @@ angular.module('mm.core.login', [])
     .state('mm_login.site', {
         url: '/site',
         templateUrl: 'core/components/login/templates/site.html',
-        controller: 'mmLoginSiteCtrl'
+        controller: 'mmLoginSiteCtrl',
+		onEnter: function($state){
+			$state.go('mm_login.credentials',{siteurl:'http://lms.iutoic-dhaka.edu'});
+		}
     })
     .state('mm_login.credentials', {
         url: '/cred',
